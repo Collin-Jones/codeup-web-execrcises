7048b7e06cb96b00cc7329de1686cb61f386c2d7
Collin Jones <collin.g.jones34@gmail.com>
2021-12-08 11:59:38 -0600

    adds hello-asteroid

---
+++ JS/hello-asteroid.js
@@ -0,0 +1,4 @@
+//console.log("I love to eat sushi." + " I love pizza but it has to be made with fresh ingredients or I won't eat it");
+favoritefood = "Sushi";
+var phrase1 = "My favorite food is ";
+console.log(phrase1 + favoritefood)
