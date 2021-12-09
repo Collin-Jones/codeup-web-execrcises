42414978d9a2bcf1a979815dd259bb31412fbbe6
Collin Jones <collin.g.jones34@gmail.com>
2021-12-09 11:51:38 -0600

    Operators.js

--- JS/PrimitiveTypes.js
+++ JS/PrimitiveTypes.js
@@ -1,3 +1,7 @@
-console.log(99.9);
-
-
+console.log(typeof 99.9);
+console.log(typeof "false");
+console.log(typeof false);
+console.log(typeof '0');
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof 'true');
