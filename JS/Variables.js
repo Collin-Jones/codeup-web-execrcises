524f37eab0345aeb92cfbd787d383d051cc399c2
Collin Jones <collin.g.jones34@gmail.com>
2021-12-09 13:00:13 -0600

    Variables.js

---
+++ JS/Variables.js
@@ -0,0 +1,15 @@
+var a = 1;
+console.log(a);
+var b = a++;
+console.log(b);
+var c = ++a;
+console.log(c);
+// what is the value of a, b, and c?
+
+var d = "hello";
+var e = false;
+console.log(d++);
+console.log(e++);
+d++;
+e++;
+
