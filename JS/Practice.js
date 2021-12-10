4c2a062043808735fb15609e8d7ee0e823b148cb
Collin Jones <collin.g.jones34@gmail.com>
2021-12-09 21:09:54 -0600

    Variables.js

--- JS/Practice.js
+++ JS/Practice.js
@@ -17,9 +17,34 @@
 //
 // console.log(Math.max(discountedAmount));
 
-let intStr = "7.51";
-console.log(parseInt(intStr));
+// let intStr = "7.51";
+// console.log(parseInt(intStr));
 // parseInt always rounds down.
 // NaN is a number, a special number
 // null
 // undefined
+
+// let age = 22
+// let hasDriverLicense = true;
+// let moneyInWallet = 10.00;
+//
+// // they must be at least 21 yrs old
+// // and have their driver license
+// // and have at least 5 dollars
+// let canGetADrink = (age >= 21)
+//     && (hasDriverLicense)
+//     && (moneyInWallet >= 5.00)
+//
+// console.log(canGetADrink)
+
+
+// let didTheDishes = true;
+// let vacuumed = false;
+// let cleanedTheRoom = false;
+//
+// if (didTheDishes || vacuumed || cleanedTheRoom){
+//     console.log("Thank God, we're making progress")
+// }else{
+//     console.log("Why did I have kids")
+// }
+
--- JS/Variables.js
+++ JS/Variables.js
@@ -1,3 +1,4 @@
+//Exercises 1
 var a = 1;
 console.log(a);
 var b = a++;
@@ -10,8 +11,96 @@ var d = "hello";
 var e = false;
 console.log(d++);
 console.log(e++);
-d++;
-e++;
+    d++;
+    e++;
+
+// Exercise 2
+let sample = "\nHello Codeup";
+{
+    sample = sample.length;
+}
+console.log(sample)
+let sameple = "Hello Codeup";
+{
+sameple = sameple.toUpperCase()
+}
+console.log(sameple)
+
+
+sameple = "\nHello Codeup";
+{
+    sameple = sameple.toLowerCase()
+}
+console.log(sameple)
+
+sample = "\nHello Codeup ";
+{
+    console.log(sample + "Students.")
+}
+
+sample = "\nHello Codeup ";
+{
+    console.log(sample + "Class")
+}
+
+sample = "\nHello Codeup";
+{
+    console.log(sample.indexOf("c"))
+}
+
+sample = "\nHello Codeup";
+{
+    console.log(sample.indexOf("C"))
+}
+
+sample = "\nHello Codeup";
+{
+    console.log(sample.substring(6, 13))
+}
 
 
 
+//Exercise 3
+// Renting movies for a certain amount of days
+let mermaid = 3
+let brotherBear = 5
+let hercules = 1
+    sum = (mermaid + brotherBear + hercules);
+// $3 per day
+let t = sum * 3;
+console.log("\nTo be able to keep these movies you must pay $" + t + " per day." );
+
+//How much payment would you receive in payment for this week
+let Facbook = 10 * 350 , Google = 6 * 400, Amazon = 4 * 380;
+let total = (Facbook + Google + Amazon);
+console.log("\nThe payment amount comes out to " + total + ".");
+
+// Trying to enroll in a class
+let student = 1 , enrolled = 15 , course = student + enrolled;
+if (course > 17){
+    outcome = "\nAll seats are filled. Choose a different class."
+}else if (course < 17) {
+    outcome = "\nYou have been placed into the class!"
+}
+console.log(outcome)
+
+let product = 3, discount = .50, items = 4, price = 3
+let equal;
+let pay
+if(product < items){
+    pay = (price * discount);
+    equal = price - pay;
+    console.log("They bought 4 items so they receive 50 percent discount, their total is " + equal + ".")
+} else {
+    equal = price
+}
+
+// Exercise 4
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+if ((password.includes(username)) && (password.length > 5) && (username.length <= 20)){
+    console.log("\nIncorrect format.")
+}else {
+    console.log("\n*loading screen* ")
+}
