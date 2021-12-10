1e3eae7b57fc74f4c163a222674757753359556d
Collin Jones <collin.g.jones34@gmail.com>
2021-12-10 09:39:25 -0600

    Practice.js

--- JS/Practice.js
+++ JS/Practice.js
@@ -48,3 +48,15 @@
 //     console.log("Why did I have kids")
 // }
 
+//Must have a credit score over 680 and have at least $4000 on-hand
+//OR, they must have at least $10000 on-hand
+let creditScore = 720;
+let cashOnHand = 5000;
+if ((cashOnHand >= 4000 && creditScore >= 680)){
+    console.log("\n Will be able to extend the auto loan");
+} else{
+    console.log("\n Will not be able to extend the auto loan")
+}
+
+
+
