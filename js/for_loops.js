766e46bc7a9be392c206bb232937e8df186d462c
Collin Jones <collin.g.jones34@gmail.com>
2022-01-03 19:24:04 -0600

    I'm trying

---
+++ js/for_loops.js
@@ -0,0 +1,50 @@
+// function showMultiplictionsTable(number){
+//     for (let a = 1; a <= 10; a++){
+//         console.log(number + " x " + a + " = " + (number * a));
+//     }
+// }
+// showMultiplictionsTable(7)
+
+// function randomNumber() {
+//     for (let a = 1; a <= 10; a++) {
+//         let numberV2 = Math.floor(Math.random(20) * 200)
+//         if (numberV2 % 2 === 0) {
+//             console.log(numberV2 + " is an even number.")
+//         } else if (numberV2 % 2 !== 0) {
+//             console.log(numberV2 + " is an odd number.")
+//         }
+//     }
+//
+// }
+//
+// randomNumber()
+
+
+// Wesley helped me a lot with this one. I will come back to this and try it as a switch statement
+// for (i = 1; i < 10; i++){
+//     if (i === 1){
+//         console.log(1)
+//     }else if(i === 2){
+//         console.log(22)
+//     }else if(i === 3){
+//         console.log(333)
+//     }else if(i === 4){
+//         console.log(4444)
+//     }else if (i === 5){
+//         console.log(55555)
+//     }else if (i === 6){
+//         console.log(666666)
+//     }else if(i === 7){
+//         console.log(7777777)
+//     }else if(i === 8){
+//         console.log(88888888)
+//     }else if(i === 9){
+//         console.log(999999999)
+//     }
+// }
+
+// for (i = 100; i >= 5; i -= 5){
+//     console.log(i)
+// }
+
+
