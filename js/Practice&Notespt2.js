bdb27cce2a262b33b8109df63f2df8e6644871d6
Collin Jones <collin.g.jones34@gmail.com>
2022-01-26 09:26:13 -0600

    java exercise

--- js/Practice&Notespt2.js
+++ js/Practice&Notespt2.js
@@ -487,17 +487,28 @@
 // this code will produce a console log every second
 // when count >= max, the interval is cancelled, and the logging will stop
 
-var count = 0;
-var max = 10;
-var interval = 1000; // interval time in milliseconds
+// var count = 0;
+// var max = 10;
+// var interval = 1000; // interval time in milliseconds
+//
+// var intervalId = setInterval(function () {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
 
-var intervalId = setInterval(function () {
-    if (count >= max) {
-        clearInterval(intervalId);
-        console.log('All done');
-    } else {
-        count++;
-        console.log('Repeating this line ' + count);
-    }
-}, interval);
 
+function findAverage(grades){
+    let total = 0;
+
+    grades.forEach(function (grade){
+        total = total + grade;
+    });
+    return total / grades.length;
+}
+
+console.log(findAverage([95, 74, 86, 100]));
