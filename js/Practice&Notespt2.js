eea5e6d61fcf3bc42791cea73142e94eba35945d
Collin Jones <collin.g.jones34@gmail.com>
2022-01-27 09:19:34 -0600

    morning exercise

--- js/Practice&Notespt2.js
+++ js/Practice&Notespt2.js
@@ -487,19 +487,19 @@
 // this code will produce a console log every second
 // when count >= max, the interval is cancelled, and the logging will stop
 
-var count = 0;
-var max = 10;
-var interval = 1000; // interval time in milliseconds
-
-var intervalId = setInterval(function () {
-    if (count >= max) {
-        clearInterval(intervalId);
-        console.log('All done');
-    } else {
-        count++;
-        console.log('Repeating this line ' + count);
-    }
-}, interval);
+// var count = 0;
+// var max = 10;
+// var interval = 1000; // interval time in milliseconds
+//
+// var intervalId = setInterval(function () {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
 
 
 // function findAverage(grades){
@@ -512,3 +512,9 @@ var intervalId = setInterval(function () {
 // }
 //
 // console.log(findAverage([95, 74, 86, 100]));
+
+function sumOfSquares(a, b){
+    return (a * a) + (b * b);
+}
+
+console.log(sumOfSquares(4, 2))
