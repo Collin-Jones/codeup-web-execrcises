e6f10eff6c97d7dfe36e7ca508c389b95e7d235a
Collin Jones <collin.g.jones34@gmail.com>
2022-02-17 16:58:09 -0600

    Fixing some github issues

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-let OPEN_WEATHER_KEY = 'bb29a681b2b4f0e524d0b187aa7f0160'
-let MAP_BOX_KEY = 'pk.eyJ1IjoidGhldGFjbzUwIiwiYSI6ImNremVmMmZpbTJ5YzMycnByb2VpcmYyMW4ifQ.Ehx__zRSg-2ZvDwaTIzDuw'
-let token = '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'
