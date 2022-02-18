9ae1e988ea5f91827296c28ef79baadb2d57df74
Collin Jones <collin.g.jones34@gmail.com>
2022-02-18 11:36:12 -0600

    was able to get the first part of the exercise done

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,12 @@
-const lastRepo = function (username) {
-    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `${GITHUB_TOKEN}`}})
-        .then(response => response.json())
-        .then(result => {
-            return result[0];
-        })
-
+function lastRepo(username) {
+    let gitPromise = fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `${GITHUB_TOKEN}`}})
+        gitPromise.then(response => response.json())
+        .then(event => console.log(new Date(event[0].created_at).toDateString()));
+            // return result[0];
 }
 lastRepo('Collin-Jones')
+
+
+function wait(){
+
+}
