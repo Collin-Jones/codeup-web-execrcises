04f433e42f85cf2067d73043c45f0a755e9b606e
Collin Jones <collin.g.jones34@gmail.com>
2022-02-18 12:16:02 -0600

    adding a few things

--- js/promises.js
+++ js/promises.js
@@ -1,12 +1,7 @@
 function lastRepo(username) {
-    let gitPromise = fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `${GITHUB_TOKEN}`}})
+    let gitPromise = fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `${GITHUB_TOKEN}`}});
         gitPromise.then(response => response.json())
         .then(event => console.log(new Date(event[0].created_at).toDateString()));
             // return result[0];
 }
-lastRepo('Collin-Jones')
-
-
-function wait(){
-
-}
+lastRepo('Collin-Jones');
