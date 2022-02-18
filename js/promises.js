acc0d4a59722b42079711ac73f4d03ece1e78e4b
Collin Jones <collin.g.jones34@gmail.com>
2022-02-17 22:34:57 -0600

    Trying to figure this exercise

---
+++ js/promises.js
@@ -0,0 +1,9 @@
+const lastRepo = function (username) {
+    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `${GITHUB_TOKEN}`}})
+        .then(response => response.json())
+        .then(result => {
+            return result[0].repo.name;
+        })
+
+}
+lastRepo('Collin-Jones')
